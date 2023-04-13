38299b978b06340b13510f4a6450e0909da55d83
dexter <looyatfung@gmail.com>
2023-04-13 23:01:39 +0100

    transparent background

--- lua/dexter/init.lua
+++ lua/dexter/init.lua
@@ -1,4 +1,11 @@
 require("dexter.remap")
 require("dexter.set") 
+
+-- use vimrc
 vim.cmd('source ~/.vimrc')
+
+-- use vim built in colorscheme
 vim.cmd.colorscheme('habamax')
+-- no theme background
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
