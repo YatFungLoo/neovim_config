8d9782a4dcb8750f2bd2561263687288acb864fa
dexter <looyatfung@gmail.com>
2023-04-22 10:39:09 +0100

    update

--- lua/dexter/init.lua
+++ lua/dexter/init.lua
@@ -1,11 +1,12 @@
 require("dexter.remap")
-require("dexter.set") 
+require("dexter.set")
 
 -- use vimrc
 vim.cmd('source ~/.vimrc')
 
 -- use vim built in colorscheme
+-- vim.cmd.colorscheme('gruvbox')
 vim.cmd.colorscheme('habamax')
 -- no theme background
--- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
--- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
