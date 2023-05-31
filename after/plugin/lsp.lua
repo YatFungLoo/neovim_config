79f47bee704a55b123ee8206b00459a723cb4e6c
dexter <looyatfung@gmail.com>
2023-05-31 22:41:14 +0100

    add fortls single file support

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -7,4 +7,12 @@ end)
 -- (Optional) Configure lua language server for neovim
 require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
 
+-- fortls config
+require('lspconfig').fortls.setup({
+  single_file_support = true,
+--   on_attach = function(client, bufnr)
+--     print('fortls started')
+--   end
+})
+
 lsp.setup()
